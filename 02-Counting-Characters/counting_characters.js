3fa001342ff63bd19334fd4eced91cf8852fa362
liban <botanlee515@gmail.com>
2021-02-19 13:13:10 +0100

    added

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -2,7 +2,15 @@
 /*
 
 here
+1. create a variable string and assign the prompt msg "Enter Your String".
+
+2. Print the string + string length
 
 */
 
 //insert your code below
+var string;
+
+
+string = window.prompt('Enter Your String');
+window.alert([string, string.length, null]);
